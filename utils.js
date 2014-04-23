efa00fbc41505dae07e1a7c43b7dedaabace60a8
Jonathan Wohl <jon@jonwohl.com>
2014-04-23 17:35:35 -0400

    added cli arguments for utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,12 @@
-var nycclinic = require('./data/NYCCollege.json'),
+var filename = process.argv[2],
+	type = process.argv[3];
+
+if (!filename || !type) {
+	console.log("Please specify a filename (in the data dir) and a type (e.g. 'health').");
+	process.exit();
+}
+
+var nycclinic = require('./data/'+filename),
 	mongoose = require('mongoose'),
     FacilityModel = require('./models/facility').FacilityModel,
     uuid = require('node-uuid');
@@ -10,8 +18,7 @@ function parseToFacility(pois) {
     for (var i in pois) {
         var data = pois[i],
         	nameComma = data.display_name.indexOf(','),
-        	name = data.display_name.substring(0,nameComma),
-        	type = 'education'
+        	name = data.display_name.substring(0,nameComma);
 
         var fac = new FacilityModel({
             "name": name,
