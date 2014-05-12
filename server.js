5ebb4f27dbe367495c14ad921ffbc4454b64aee4
Jonathan Wohl <jon@jonwohl.com>
2014-05-12 11:48:04 -0400

    fleshed out basic routes

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 var restify = require('restify'),
     routes = require('./routes'),
+    mongoose = require('mongoose'),
     server = restify.createServer({
         name: 'Facilitator',
         version: '0.1.1'
-    });
+    }),
+    prePath = '/api/v1';
+
+mongoose.connect('mongodb://localhost/sel');
 
 server
     .use(restify.CORS())
     .use(restify.fullResponse())
     .use(restify.bodyParser());
 
-server.get('/facilities', routes.facilities);
+// get a list of all facilities
+server.get(prePath+'/facilities', routes.facilities);
 
-server.get('/facilities/:uuid', routes.facility);
+// get a list of all facilities within radius (rad) of lat, lng
+server.get(prePath+'/facilities/near/:lat/:lng/:rad', routes.near);
 
+// get a list of all facilities within box
+server.get(prePath+'/facilities/within/:swlat/:swlng/:nelat/:nelng', routes.within);
+
+// get a single facility by id
+server.get(prePath+'/facilities/:id', routes.facility);
+
+// add a new facility
+server.post(prePath+'/facilities', routes.newFacility);
+
+// update a facility
+server.put(prePath+'/facilities/:id', routes.updateFacility);
+
+// delete a facility
+server.del(prePath+'/facilities/:id', routes.deleteFacility);
+
+// flag a facility
+server.put(prePath+'/facilities/flag/:id', routes.flagFacility);
+
+
+// Start the server.
 server.listen(3000, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
