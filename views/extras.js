a21c766d699b74da7557a6c3d33b3871de5fabf0
Abdi Dahir <abdi.dahir@outlook.com>
2014-09-22 16:41:27 -0400

    Url code mostly ported

--- views/extras.js
+++ views/extras.js
@@ -1,5 +1,7 @@
 // dependancies
 var restify = require('restify')
+var fs = require('fs')
+var mkdirp = require('mkdirp')
 
 // local includes
 var database = require('../models/dbcontroller.js')
@@ -91,6 +93,73 @@ var withinSector = function(req, res, next) {
         return next(); 
     });
 };
+
+exports.uploadPhoto = function (req, res, next) {
+    var siteId = req.params.id || null;
+
+    console.log(req.files.photo);
+    
+    // if no sideId is included in request, error
+    if (!siteId) {
+        return next(new restify.MissingParameterError("The required siteId parameter is missing."));
+    }
+
+    // make sure the id is associated with a known Site
+    database.SiteModel.findById(siteId, function (err, site) { 
+        if (err) return next(new restify.ResourceNotFoundError(JSON.stringify(err)));
+
+        console.log(site);
+
+        // move the uploaded photo from the temp location (path property) to its final location
+        fs.readFile(req.files.photo.path, function (err, data) {
+    		if (err) {
+    			console.log(err);
+    		}
+            // excuse the dir hack
+            var rootPath = __dirname.replace("views",""); + "/public/sites/photos",
+                siteDir = siteId,
+                filePath = req.files.photo.name,
+                fullPath = rootPath + '/' + siteDir + '/' + filePath;
+
+            // create the dir for the site
+            mkdirp(rootPath + '/' + siteDir, function (err) {
+                if (err) {
+                    console.error(err);
+                    return next(new restify.InternalError(JSON.stringify(err)));
+                } else {
+                    fs.writeFile(fullPath, data, function (err) {
+                        console.log('writeFile callback');
+            			if (err) {
+            				console.log('write error: ' + err);
+            				return next(new restify.InternalError(JSON.stringify(err)));
+            			}
+
+                        var url = 'http://' + req.header('Host') + '/sites/photos/' + siteDir + '/' +  filePath;
+
+                        // check that this photo is new. we'll replace the tmp path with the url
+                        var index = site.properties.photoUrls.indexOf('tmp/'+req.files.photo.name);
+                        if (index != -1) {
+                            site.properties.photoUrls.splice(index, 1, url);
+                            // site.properties.photoUrls.push(url);
+                        }
+
+                        console.log('site photo url: ' + url);
+			
+                        site.save(function (err, updatedSite, numberAffected) {
+                            if (err) {
+                				console.log('save error: ' + err);
+                				return next(new restify.InternalError(JSON.stringify(err)));
+            				}
+                            console.log('site saved, sending response');
+				            // no error, send success
+                            res.send(updatedSite);
+                        });
+                    });
+                }
+            });
+        });
+    });
+}
 
 // exports
 exports.within = within
