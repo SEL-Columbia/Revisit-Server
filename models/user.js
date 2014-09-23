e04e8963749c81bb0d565c681336e988d8218464
Abdi Dahir <abdi.dahir@outlook.com>
2014-09-23 16:22:55 -0400

    Updated basic auth stuff to hash password and to store in db. Implemented restful way to add user (although requires there to be a user to be called ...) and restful login credentials checker [Finishes #78915182]

---
+++ models/user.js
@@ -0,0 +1,95 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+// TODO: Not defined here
+var site_prefix = "http://localhost:3000/api/v0/facilities/"
+
+var SHA2 = new (require('jshashes').SHA512)()
+
+var UserModel = new Schema({
+        username: {
+            type: String,
+            index: {
+                    unique: true
+            },
+            required: true
+        },
+        
+        password: {
+            type: String,
+            required: true
+        },
+
+        salt: {
+            type: String,
+            required: true
+        }
+    } 
+);
+
+var genHash = function(pass, salt) {
+    return SHA2.b64_hmac(pass, salt);
+}
+
+var genSalt = function() {
+    // random str generator
+    return "4"
+}
+
+UserModel.statics.login = function(username, pass, callback) {
+    if (username && pass) {
+        console.log(">>> Trying:", username, pass)
+        this.findOne({"username": username}).exec(function(err, user) {
+            //console.log("Querying:", user);
+            if (err || !user) {
+                console.log("User not found");
+                callback(false);
+                return;
+            }
+
+            var salt = user.salt;
+            var hashed = user.password;
+            var new_hash = genHash(pass, salt);
+            console.log("Found: hash,salt,new", hashed, salt, new_hash);
+
+            if (hashed != new_hash) {
+                console.log("Incorrect password");
+                callback(false);
+                return;
+            }
+
+            console.log("Success!");
+            callback(true);
+            return;
+        }); 
+
+        return;
+    }
+
+    console.log("fields empty");
+    callback(false);
+    return;
+}
+
+UserModel.statics.addUser = function(username, pass, callback) {
+    console.log("Adding User:", username)
+    var salt = genSalt();
+    var hash = genHash(pass, salt);
+    console.log(hash, salt);
+    var userObj = new this({username: username, password: hash, salt: salt});
+    userObj.save(function(err, user) {
+        if (err) {
+            console.log(err);
+            callback(false);
+            return;
+        } 
+        console.log(user);
+        console.log("Success!");
+        callback(true);
+    });
+
+    return;
+}
+
+exports.UserModel = mongoose.model('UserModel', UserModel, 'authentication');
