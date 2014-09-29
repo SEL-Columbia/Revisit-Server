ad64ecbaf268dee2e2be60d6de6bcb358b7bd7e9
Abdi Dahir <abdi.dahir@outlook.com>
2014-09-29 16:37:04 -0400

    added surprisingly neccassary 'done' function param

--- test/extra.js
+++ test/extra.js
@@ -9,55 +9,55 @@ describe('API Extra Routes', function() {
     });
     
     describe('#near', function() {
-        it('should return facilties with 100km', function() {
+        it('should return facilties with 100km', function(done) {
             assert(false);
         });
 
-        it('should return facilities within 100mi', function() {
+        it('should return facilities within 100mi', function(done) {
             assert(false);
         });
 
-        it('should return facilities within default 10mi', function() {
+        it('should return facilities within default 10mi', function(done) {
             assert(false);
         });
         
-        it('should return no facilities within 0km', function() {
+        it('should return no facilities within 0km', function(done) {
             assert(false);
         });
         
-        it('should return no facilities', function() {
+        it('should return no facilities', function(done) {
             assert(false);
         });
     });
 
     describe('#within', function() {
         it('should return facilties within box defined by x,y and x",y"', 
-        function() {
+        function(done) {
             assert(false);
         });
 
         it('should return no facilties within box a,b and a,b (point)', 
-        function() {
+        function(done) {
             assert(false);
         });
 
-        it('should return no facilities', function() {
+        it('should return no facilities', function(done) {
             assert(false);
         });
     });
 
     describe('#withinSector', function() {
         it('should return facilties within box x,y and x",y" and sector', 
-        function() {
+        function(done) {
             assert(false);
         });
 
         it('should return no facilties within box a,b and a,b (point) and sec', 
-        function() {
+        function(done) {
             assert(false);
         });
 
-        it('should return no facilities', function() {
+        it('should return no facilities', function(done) {
             assert(false);
         });
     });
@@ -65,26 +65,26 @@ describe('API Extra Routes', function() {
     describe('#uploadPhoto', function() {
         it('should upload a photo to /public/sites/photos/$id/name and the url'
                 + ' should be added to the facility', 
-        function() {
+        function(done) {
             assert(false);
         });
 
         it('should upload a photo to /public/sites/photos/$id/name with the'
                 + 'same name as previous entry and the url should NOT be added'
                 + ' to the facility as it already exists', 
-        function() {
+        function(done) {
             assert(false);
         });
 
         it('should upload a photo to /public/sites/photos/$id/name and the url'
                 + ' should be added to the facility after creating the'
                 + ' properties field for the facility which did not have one', 
-        function() {
+        function(done) {
             assert(false);
         });
 
         it('should not find the facility and not store the uploaded photo', 
-        function() {
+        function(done) {
             assert(false);
         });
     });
