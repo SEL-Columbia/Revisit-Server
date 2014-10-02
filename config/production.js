3d0708bfc1e6b14c573b7e4382ffa4ba14ea99e1
Jonathan Wohl <jon@jonwohl.com>
2014-10-02 12:14:07 -0400

    refactored default_config to separate module

--- config/production.js
+++ config/production.js
@@ -1,16 +1,22 @@
-console.log('LOADING PRODUCTION CONFIG');
+console.log('LOADING STAGING CONFIG');
+
+var _ = require('lodash-node'),
+	def_config = require('./default_config');
 
 var config = {};
 
 config.prePath = '/api/v0';
 config.host = 'revisit.global';
 config.port = '';
-config.site = "http://" + def_config.host;
-config.site += config.port ? ":" + def_config.port : '';
-config.site += def_config.prePath + "/" + "facilities/";
+config.site = "http://" + config.host;
+config.site += config.port ? ":" + config.port : '';
+config.site += config.prePath + "/" + "facilities/";
 
-def_config.USE_AUTH = false;
+config.USE_AUTH = false;
 
-config.log_root = '/var/log/' + exports.app_name + '/';
+config.log_root = '/var/log/' + def_config.app_name + '/';
+
+// add default config to this env config
+_.defaults(config, def_config);
 
 module.exports = config;
