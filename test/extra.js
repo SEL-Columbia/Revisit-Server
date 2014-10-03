bc10735e516cd9a4ba92418aeab2410b22eb6101
Abdi Dahir <abdi.dahir@outlook.com>
2014-10-03 09:46:24 -0400

    Finished extra functionality test cases, just missing photo testing

--- test/extra.js
+++ test/extra.js
@@ -1,91 +1,216 @@
 var assert = require('assert');
+var request = require('supertest');
+var should = require('should');
+var _ = require('lodash');
 var db_controller = require('./../models/dbcontroller.js');
 
+
+
 describe('API Extra Routes', function() {
-    var url = "localhost:3000/api/v0/facilities";
+    var url = "http://localhost:3000/api/v0/";
     before(function(done) {
-        //var db = db_controller.connect();
         done();
     });
     
     describe('#near', function() {
-        it('should return facilties with 100km', function(done) {
-            assert(false);
+        it('should return facilties with 1km', function(done) {
+            request(url)
+                .get("facilities/near/40.7645704/-73.9570783/1/km")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(13);
+                    res.body.facilities.length.should.be.above(1);
+                    done();
+                });
         });
 
-        it('should return facilities within 100mi', function(done) {
-            assert(false);
+        it('should return facilities within 1mi', function(done) {
+            request(url)
+                .get("facilities/near/40.7645704/-73.9570783/1")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(16);
+                    res.body.facilities.length.should.be.above(1);
+                    done();
+                });
+
         });
 
-        it('should return facilities within default 10mi', function(done) {
-            assert(false);
+        it('should return 1 facilities within 0km', function(done) {
+             request(url)
+                .get("facilities/near/40.7645704/-73.9570783/0/km")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(1);
+                    done();
+                });
         });
-        
-        it('should return no facilities within 0km', function(done) {
-            assert(false);
-        });
+
         
         it('should return no facilities', function(done) {
-            assert(false);
+             request(url)
+                .get("facilities/near/0.0/0.0/0/km")
+                .expect('Content-Type', /json/)
+                .expect(404) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.code.should.match("Not Found");
+                    done();
+                });
         });
     });
 
     describe('#within', function() {
         it('should return facilties within box defined by x,y and x",y"', 
         function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/0/-180/90/0")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(196);
+                    done();
+                });
+
         });
 
         it('should return no facilties within box a,b and a,b (point)', 
         function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/40.7645704/-73.9570783/40.7645704/-73.9570783")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(1);
+                    done();
+                });
         });
 
         it('should return no facilities', function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/0/0/0/0")
+                .expect('Content-Type', /json/)
+                .expect(404) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.code.should.match("Not Found");
+                    done();
+                });
         });
     });
 
     describe('#withinSector', function() {
         it('should return facilties within box x,y and x",y" and sector', 
         function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/0/-180/90/0/health")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(76);
+                    done();
+                });
+
         });
 
         it('should return no facilties within box a,b and a,b (point) and sec', 
         function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/40.7645704/-73.9570783/40.7645704/-73.9570783/health")
+                .expect('Content-Type', /json/)
+                .expect(200) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.facilities.should.be.ok;
+                    res.body.facilities.should.have.lengthOf(1);
+                    done();
+                });
         });
 
         it('should return no facilities', function(done) {
-            assert(false);
+            request(url)
+                .get("facilities/within/0/0/0/0/health")
+                .expect('Content-Type', /json/)
+                .expect(404) 
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.code.should.match("Not Found");
+                    done();
+                });
         });
     });
 
-    describe('#uploadPhoto', function() {
-        it('should upload a photo to /public/sites/photos/$id/name and the url'
-                + ' should be added to the facility', 
-        function(done) {
-            assert(false);
-        });
-
-        it('should upload a photo to /public/sites/photos/$id/name with the'
-                + 'same name as previous entry and the url should NOT be added'
-                + ' to the facility as it already exists', 
-        function(done) {
-            assert(false);
-        });
-
-        it('should upload a photo to /public/sites/photos/$id/name and the url'
-                + ' should be added to the facility after creating the'
-                + ' properties field for the facility which did not have one', 
-        function(done) {
-            assert(false);
-        });
-
-        it('should not find the facility and not store the uploaded photo', 
-        function(done) {
-            assert(false);
-        });
-    });
+//    describe('#uploadPhoto', function() {
+//        it('should upload a photo to /public/sites/photos/$id/name and the url'
+//                + ' should be added to the facility', 
+//        function(done) {
+//            assert(false);
+//        });
+//
+//        it('should upload a photo to /public/sites/photos/$id/name with the'
+//                + 'same name as previous entry and the url should NOT be added'
+//                + ' to the facility as it already exists', 
+//        function(done) {
+//            assert(false);
+//        });
+//
+//        it('should upload a photo to /public/sites/photos/$id/name and the url'
+//                + ' should be added to the facility after creating the'
+//                + ' properties field for the facility which did not have one', 
+//        function(done) {
+//            assert(false);
+//        });
+//
+//        it('should not find the facility and not store the uploaded photo', 
+//        function(done) {
+//            assert(false);
+//        });
+//    });
+//
 });
