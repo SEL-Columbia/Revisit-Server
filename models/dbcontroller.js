ed56794b64a2c4e58137ed1a4e2b9410b3c9da6a
Jonathan Wohl <jon@jonwohl.com>
2014-10-08 16:57:40 -0400

    removed unnecessary code

--- models/dbcontroller.js
+++ models/dbcontroller.js
@@ -8,19 +8,14 @@ var log = require('./../log/logger.js').log;
 var conf = require('./../config/db/db_config');
 
 // db 
-var connect = function(db_loc) {
-    // var db_location = db_loc || 'localhost/sel';
-    // log.info(conf);
+var connect = function() {
     mongoose.connect(conf.uri, conf.options);
 	
     var db = mongoose.connection;
-    // var db = mongoose.createConnection('mongodb://test:test@localhost/sel');
     db.on('error', console.error.bind(console, 'connection err:'));
     db.once('open', function() {
         log.info('Connected To Mongo Database at ' + conf.uri);
     });
-
-    // db.user = UserModel;
 
     return db;
 };
